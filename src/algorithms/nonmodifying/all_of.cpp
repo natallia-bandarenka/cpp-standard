9244dc81545cd3e62d79c53619a3e4a5ffe80bf6
fvarose <fva.rose@gmail.com>
2018-10-14 20:35:05 +0200

    Add test cases for std::all_of.

---
+++ src/algorithms/nonmodifying/all_of.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+
+#include <algorithm>
+#include <vector>
+
+TEST_CASE("all_of") {
+  SECTION("all_of returns true if all elements of a collection satisfy a "
+          "predicate") {
+    auto predicate = [](int i) { return i < 4; };
+    auto collection = std::vector<int>{0, 1, 2, 3};
+
+    CHECK(std::all_of(collection.cbegin(), collection.cend(), predicate));
+  }
+
+  SECTION("all_off returns false if at least one element of a collection does "
+          "not satisfy a predicate") {
+    auto predicate = [](int i) { return i < 4; };
+    auto collection = std::vector<int>{0, 1, 2, 3, 4};
+
+    CHECK_FALSE(std::all_of(collection.cbegin(), collection.cend(), predicate));
+  }
+}
