914c9846c494301edfefffc29012fb4c5cbf6a13
fvarose <fva.rose@gmail.com>
2018-10-14 19:23:15 +0200

    Add catch2 submodule and dummy test case.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
-int main() {
-    return 0;
+#define CATCH_CONFIG_MAIN // catch provides the main function
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Catch is setup correctly")
+{
+    CHECK(1 + 1 == 2);
 }
