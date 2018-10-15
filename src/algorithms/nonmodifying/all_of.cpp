a48355ffd744775325c55b58c0799a8b4c5f5aea
fvarose <fva.rose@gmail.com>
2018-10-15 20:19:46 +0200

    Fix formatting and typo.

--- src/algorithms/nonmodifying/all_of.cpp
+++ src/algorithms/nonmodifying/all_of.cpp
@@ -3,17 +3,20 @@
 #include <algorithm>
 #include <vector>
 
-TEST_CASE("all_of") {
+TEST_CASE("all_of")
+{
   SECTION("all_of returns true if all elements of a collection satisfy a "
-          "predicate") {
+          "predicate")
+  {
     auto predicate = [](int i) { return i < 4; };
     auto collection = std::vector<int>{0, 1, 2, 3};
 
     CHECK(std::all_of(collection.cbegin(), collection.cend(), predicate));
   }
 
-  SECTION("all_off returns false if at least one element of a collection does "
-          "not satisfy a predicate") {
+  SECTION("all_of returns false if at least one element of a collection does "
+          "not satisfy a predicate")
+  {
     auto predicate = [](int i) { return i < 4; };
     auto collection = std::vector<int>{0, 1, 2, 3, 4};
 
